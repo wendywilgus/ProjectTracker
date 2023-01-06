db052facad9f2a94f81de869c1888d9eaf088b1a
Wendy Wilgus <wilguswendy@gmail.com>
2023-01-06 14:34:54 -0500

    added react and like button

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,80 @@
-const moment = require("moment");
+// const moment = require("moment");
 
-let timeDisplay = $('#time-display');
-let projectModal = $('#project-modal');
-let projectForm = $('#project-form');
-let projectName = $('#project-name-input');
-let projectType = $('#project-type-input');
-let hourlyRate = $('#hourly-rate-input');
-let dueDate = $('#due-date-input');
+let timeDisplayEl = $('#time-display');
+let projectModalEl = $('#project-modal');
+let projectFormEl = $('#project-form');
+let projectNameInputEl = $('#project-name-input');
+let projectTypeInputEl = $('#project-type-input');
+let hourlyRateInputEl = $('#hourly-rate-input');
+let dueDateInputEl = $('#due-date-input');
+let projectDisplayEl = $('#project-display');
+
 
 
 //display date and time using Moment.js
 function displayTime() {
     let rightNow = moment().format('MMMM Do YYYY, h:mm:ss a');
-    timeDisplay.text(rightNow);
+    timeDisplayEl.text(rightNow);
+    console.log("displayTime");
 }
 
 function printData(name, type, hourlyRate, dueDate) {
     let projectRow = $('<tr>');
-    let projectNameEl = $('<td>').addClass('p-2').text(name);
-    let projectTypeEl = $('<td>').addClass('p-2').text(type);
-    let hourlyRateEl = $('<td>').addClass('p-2').text(hourlyRate);
+    let projectNameTdEL = $('<td>').addClass('p-2').text(name);
+    let projectTypeTdEl = $('<td>').addClass('p-2').text(type);
+    let rateTdEl = $('<td>').addClass('p-2').text(hourlyRate);
 
-    let dueDateEl = $('<td>').addClass('p-2').text(dueDate);
+    let dueDateTdEl = $('<td>').addClass('p-2').text(dueDate);
     let daysToDate = moment(dueDate, 'MM/DD/YYY').diff(moment(), 'days');
-    let daysRemaining = $('<td>').addClass('p-2').text(daysToDate);
+    let daysRemainingTdEl = $('<td>').addClass('p-2').text(daysToDate);
 
     let totalEarnings = calculateTotalEarnings(hourlyRate, daysToDate);
 
     let totalTdEl = $('<td>').addClass('p-2').text('$' + totalEarnings);
 
-    let deleteProject = $('<td>').addClass('p-2').text('X');
+    let deleteProject = $('<td>').addClass('p-2 delte-project-btn text-center').text('X');
 
     projectRow.append(
-        projectNameEl,
-        projectTypeEl,
-        hourlyRateEl,
-        dueDateEl,
-        daysRemaining,
+        projectNameTdEL,
+        projectTypeTdEl,
+        rateTdEl,
+        dueDateTdEl,
+        daysRemainingTdEl,
         totalTdEl,
         deleteProject,
     );
+
+    projectDisplayEl.append(projectRow);
+
+    projectModalEl.modal('hide');
 }
 
+function calculateTotalEarnings(rate, days) {
+    let dailyTotal = rate * 8;
+    let total = dailyTotal * days;
+    return total;
+}
+
+function handleDelete(event) {
+    let btnClicked = $(event.target);
+    btnClicked.parent('tr').remove();
+}
+
+function handleProjectSubmission(event) {
+    event.preventDefault();
+
+    let projectName = projectNameInputEl.val().trim();
+    let projectType = projectTypeInputEl.val().trim();
+    let hourlyRate = hourlyRateInputEl.val().trim();
+    let dueDate = dueDateInputEl.val().trim();
+
+    printData(projectName, projectType, hourlyRate, dueDate);
+
+    projectFormEl[0].reset();
+}
+
+projectFormEl.on('submit', handleProjectSubmission);
+projectDisplayEl.on('click', '.delete-project-btn', handleDelete);
+dueDateInputEl.datepicker({ minDate: 1 });
 
 setInterval(displayTime, 1000);
